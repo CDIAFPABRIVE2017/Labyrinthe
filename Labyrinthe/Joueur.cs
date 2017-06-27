930bbc46d656e4ec89ee37d9a51cd464d73d7be2
Dami <dami.bachelet@gmail.com>
2017-06-27 16:27:24 +0200

    premiers essais
    Vers l'infini et au delà...

--- Labyrinthe/Joueur.cs
+++ Labyrinthe/Joueur.cs
@@ -14,69 +14,78 @@ namespace Labyrinthe
         decimal _vitesse, _force;
         Inventaire _inventaire;
 
+        enum Direction
+        {
+            HAUT = 0,
+            DROITE = 1,
+            BAS = 2,
+            GAUCHE = 3
+        }
+
+        void Deplacmeent()
+        {
+            Direction d = Direction.DROITE;
+            if (ChangementCase(d))
+            {
+                // Modif position
+                // Interroger server
+            }
+        }
+
+        /// <summary>
+        /// Retourne si le mouvement peut être fait
+        /// </summary>
+        /// <param name="d">Direction</param>
+        /// <returns></returns>
+        bool ChangementCase(Direction d)
+        {
+            switch (d)
+            {
+                case Direction.HAUT:
+                    // Aller en hauf
+                    // Appeler le Laby local pour les murs
+                    break;
+                case Direction.DROITE:
+                    // Aller à droite
+                    break;
+                case Direction.BAS:
+                    // Aller en bas
+                    break;
+                case Direction.GAUCHE:
+                    // Aller à gauche
+                    break;
+            }
+            return true;
+        }
+
         public Point Position
         {
-            get
-            {
-                return _position;
-            }
-
-            set
-            {
-                _position = value;
-            }
+            get { return _position; }
+            set { _position = value; }
         }
 
         public int Vision
         {
-            get
-            {
-                return _vision;
-            }
-
-            set
-            {
-                _vision = value;
-            }
+            get { return _vision; }
+            set { _vision = value; }
         }
 
         public decimal Vitesse
         {
-            get
-            {
-                return _vitesse;
-            }
-
-            set
-            {
-                _vitesse = value;
-            }
+            get { return _vitesse; }
+            set { _vitesse = value; }
         }
 
         public decimal Force
         {
-            get
-            {
-                return _force;
-            }
-
-            set
-            {
-                _force = value;
-            }
+            get { return _force; }
+            set { _force = value; }
         }
 
         internal Inventaire Inventaire
         {
-            get
-            {
-                return _inventaire;
-            }
-
-            set
-            {
-                _inventaire = value;
-            }
+            get { return _inventaire; }
+            set { _inventaire = value;}
         }
     }
 }
