6bdd9cd3b095e3576485145c16f3febfe7e1d1eb
Thomas-Cdi <staub.thomas87@gmail.com>
2017-07-04 10:44:23 +0200

    tt

--- Labyrinthe/Partie.cs
+++ Labyrinthe/Partie.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using MazeDll;
+using ReseauDLL;
 
 namespace Labyrinthe
 {
@@ -12,76 +13,108 @@ namespace Labyrinthe
     {
 
         public static int hauteur = 51, largeur = 51;
-        public static bool _isClient;
-        public MyLabyrinthe laby;
+        public Joueur joueur = new Joueur();
+        public Reseau rezo = new Reseau();
 
-        public bool IsClient
-        {
-            get
-            {
-                return _isClient;
-            }
 
-            set
-            {
-                _isClient = value;
-            }
-        }
 
           public void Lancement()
         {
-            // Broadcast "serveur ?"
-            //attend la réponse true sec
-
-            IsClient = false;
-            /*if (false)
-                IsClient = true;
-            else
-                IsClient = false;*/
-
-            laby = ConstructionLabyrinthe();
-            laby.NouveauxObjets();
+            rezo.DataReceived += Reception;
+            joueur.Laby = ConstructionLabyrinthe();
+            joueur.InitialisationCarte();
+            askPosition();
         }
-
-        public static MyLabyrinthe ConstructionLabyrinthe()
+        #region Réception et réactions
+        private void Reception(string sender, object data)
         {
-            /*    if (IsClient)
-                      //On interroge le serveur et on récupère son labyrinthe
-                  else
-                  {}
-            */
 
-            MyLabyrinthe laby = new Labyrinthe.MyLabyrinthe();
-
-            int[,] tempMaze = new int[hauteur, largeur];
-
-            Maze.InitialiseTableau(tempMaze, hauteur, largeur);
-
-            Maze.GenereCheminPrimaire(tempMaze, hauteur, largeur, 1, 0);
-
-            laby.ConversionMaze(tempMaze);
-
-            return laby;
-        }
-
-        public Loot TryRamassageObjet(Joueur joueur)
-        {
-            if (IsClient)
-                //Interrogation serveur et récupération du LOOT
-                return null;
-            else
+            //Si on reçoit un point
+            if (data.GetType() == typeof(Point))
             {
-                Loot loot;
-                if (joueur.Laby.Liste.TryGetValue(joueur.Position.ToString(), out loot))
+                //On enlève l'objet à l'endroit correspondant
+                Loot loot = TryRamassageObjet(joueur.Laby, (Point)data);
+                //Et si on est serveur on envoie le point pour que les autres joueurs l'enlèvent, et on envoie la position du joueur pour qu'elle soit actualisée chez les autres
+                if (rezo.IsServer)
                 {
-                    joueur.Laby.Liste.Remove(joueur.Position.ToString());
-                    
-                    //Prévenir les autres de remove cet objet !
-                    return loot;
+                    if (loot != null)
+                        rezo.SendData((Point)data);
+                    rezo.SendData(new PositionJoueur(sender, (Point)data));
                 }
 
-                else return null;
-            }            
+            }
+
+            else if (data.GetType() == typeof(MyLabyrinthe))
+                joueur.Laby = (MyLabyrinthe)data;
+
+            else if (data.GetType() == typeof(string))
+            {
+                if ((string)data == "Labyrinthe")
+                    SendLabyrinthe(sender);
+                else if ((string)data == "Position")
+                { }
+            }
+
+            else if (data.GetType() == typeof(PositionJoueur))
+                if (joueur.Position == new Point(0, 0))
+                    joueur.Position = ((PositionJoueur)data).Position;
+                else
+                    UpdatePositions((PositionJoueur)data);
+
+
+                    
         }
+        public void askPosition()
+        {
+            rezo.SendData("Position");
+        }
+
+        private void SendLabyrinthe(string sender)
+        {
+            rezo.SendData(joueur.Laby, sender);
+        }
+
+        private void UpdatePositions(PositionJoueur posJ)
+        {
+            joueur.Laby.Joueurs.Remove(posJ.Position);
+            joueur.Laby.Joueurs.Add(posJ.Position, posJ.Adresse);
+        }
+        #endregion
+
+        public MyLabyrinthe ConstructionLabyrinthe()
+        {
+            MyLabyrinthe laby = new Labyrinthe.MyLabyrinthe();
+            if (rezo.IsServer)
+            {
+                int[,] tempMaze = new int[hauteur, largeur];
+
+                Maze.InitialiseTableau(tempMaze, hauteur, largeur);
+
+                Maze.GenereCheminPrimaire(tempMaze, hauteur, largeur, 1, 0);
+
+                laby.ConversionMaze(tempMaze);
+
+                laby.CreationListDico();
+
+                return laby;
+            }
+            else
+            {
+                //demande un laby au serveur
+                rezo.SendData("Labyrinthe");
+                return laby;
+            }
+        }
+
+        public Loot TryRamassageObjet(MyLabyrinthe laby, Point point)
+        {
+            Loot loot;
+            if (laby.Liste.TryGetValue(point.ToString(), out loot))
+            {
+                laby.Liste.Remove(point.ToString());
+                return loot;
+            }
+            else return null;
+        }            
     }
 }
